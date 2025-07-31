ee38aa0f07cb57e61b6d8211efe165d138dd47bc
zaindev81 <zaindev81@gmail.com>
2025-07-31 11:04:51 +0700

    tokio redis

--- axum/tokio-redis/src/main.rs
+++ axum/tokio-redis/src/main.rs
@@ -3,9 +3,11 @@ use axum::{
     http::StatusCode,
     Json, Router,
 };
-use serde::{Deserialize, Serialize};
-use tracing::debug;
-use tracing_subscriber::prelude::*;
+use bb8::{Pool, PooledConnection};
+use bb8_redis::RedisConnectionManager;
+use redis::AsyncCommands;
+use tracing::{debug};
+use tracing_subscriber::{field::debug, prelude::*};
 
 #[tokio::main]
 async fn main() {
@@ -18,15 +20,30 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
+    debug!("connecting to redis");
+
+    let manager = RedisConnectionManager::new("redis://localhost").unwrap();
+    let pool = bb8::Pool::builder().build(manager).await.unwrap();
+
+    {
+        // ping the database before starting
+        let mut conn = pool.get().await.unwrap();
+        conn.set::<&str, &str, ()>("foo", "bar").await.unwrap();
+        let result: String = conn.get("foo").await.unwrap();
+        assert_eq!(result, "bar");
+    }
+
+    debug!("successfully connected to redis and pinged it");
+
     // build our application with a route
     let app = Router::new()
-        // `GET /` goes to `root`
         .route("/", get(root))
-        // `POST /users` goes to `create_user`
-        .route("/users", post(create_user));
+        .with_state(pool);
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .unwrap();
     debug!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
@@ -34,33 +51,4 @@ async fn main() {
 // basic handler that responds with a static string
 async fn root() -> &'static str {
     "Hello, World!"
-}
-
-async fn create_user(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
-    Json(payload): Json<CreateUser>,
-) -> (StatusCode, Json<User>) {
-    // insert your application logic here
-    let user = User {
-        id: 1337,
-        username: payload.username,
-    };
-
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
-    (StatusCode::CREATED, Json(user))
-}
-
-// the input to our `create_user` handler
-#[derive(Deserialize)]
-struct CreateUser {
-    username: String,
-}
-
-// the output to our `create_user` handler
-#[derive(Serialize)]
-struct User {
-    id: u64,
-    username: String,
 }
